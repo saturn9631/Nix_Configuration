a8c37d0db0f2dd720290db29a2fa0ed0be546557
Shakari Wade <saturn.kari.9@gmail.com>
2025-05-25 16:08:35 -0700

    Added aliases to home manager.

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -1,37 +1,37 @@
 {
-  description = "Home Manager configuration of saturnfulcrum";
+	description = "Home Manager configuration of saturnfulcrum";
+		
+	inputs = {
+	# Specify the source of Home Manager and Nixpkgs.
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+		home-manager = {
+			url = "github:nix-community/home-manager/release-24.11";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
 
-  inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+		nix-colors.url = "github:misterio77/nix-colors";
+	};
 
-    nix-colors.url = "github:misterio77/nix-colors";
-  };
-
-  outputs = { nixpkgs, home-manager, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      #Below code should be put into home manager for root user.
-      #nixosConfigurations.shakarisviewerofcarnage = lib.nixosSystem {
-      #  inherit system;
-      #  modules = [ ./configuration.nix ];
-      #}
-      homeConfigurations."saturnfulcrum" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-	extraSpecialArgs = { inherit inputs; };
-        modules = [ ./home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-      };
-    };
+	outputs = { nixpkgs, home-manager, ... }@inputs:
+	let
+		system = "x86_64-linux";
+		pkgs = nixpkgs.legacyPackages.${system};
+	in {
+	#Below code should be put into home manager for root user.
+	#nixosConfigurations.shakarisviewerofcarnage = lib.nixosSystem {
+	#  inherit system;
+	#  modules = [ ./configuration.nix ];
+	#}
+		homeConfigurations."saturnfulcrum" = home-manager.lib.homeManagerConfiguration {
+			inherit pkgs;
+			
+			# Specify your home configuration modules here, for example,
+			# the path to your home.nix.
+			extraSpecialArgs = { inherit inputs; };
+			modules = [ ./home.nix ];
+			
+			# Optionally use extraSpecialArgs
+			# to pass through arguments to home.nix
+		};
+	};
 }
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,94 +1,93 @@
 { config, pkgs, inputs, ... }:
 
 {
-  imports = [
-    inputs.nix-colors.homeManagerModules.default
-  ];
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "saturnfulcrum";
-  home.homeDirectory = "/home/saturnfulcrum";
+imports = [
+		inputs.nix-colors.homeManagerModules.default
+];
+# Home Manager needs a bit of information about you and the paths it should
+# manage.
+home.username = "saturnfulcrum";
+home.homeDirectory = "/home/saturnfulcrum";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
-  
+	# This value determines the Home Manager release that your configuration is
+	# compatible with. This helps avoid breakage when a new Home Manager release
+	# introduces backwards incompatible changes.
+	#
+	# You should not change this value, even if you update Home Manager. If you do
+	# want to update the value, then make sure to first check the Home Manager
+	# release notes.
+	home.stateVersion = "24.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = with pkgs; [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    hello
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+	# The home.packages option allows you to install Nix packages into your
+	# environment.
+	home.packages = with pkgs; [
+		# # Adds the 'hello' command to your environment. It prints a friendly
+		# # "Hello, world!" when run.
+		hello
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
+		# # It is sometimes useful to fine-tune packages, for example, by applying
+		# # overrides. You can do that directly here, just don't forget the
+		# # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+		# # fonts?
+		# (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+		# # You can also create simple shell scripts directly inside your
+		# # configuration. For example, this adds a command 'my-hello' to your
+		# # environment:
+		# (pkgs.writeShellScriptBin "my-hello" ''
+		#   echo "Hello, ${config.home.username}!"
+		# '')
+	];
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
+	# Home Manager is pretty good at managing dotfiles. The primary way to manage
+	# plain files is through 'home.file'.
+	home.file = {
+		# # Building this configuration will create a copy of 'dotfiles/screenrc' in
+		# # the Nix store. Activating the configuration will then make '~/.screenrc' a
+		# # symlink to the Nix store copy.
+		# ".screenrc".source = dotfiles/screenrc;
+		
+		# # You can also set the file content immediately.
+		# ".gradle/gradle.properties".text = ''
+		#   org.gradle.console=verbose
+		#   org.gradle.daemon.idletimeout=3600000
+		# '';
+	};
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/saturnfulcrum/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-     #EDITOR = "neovim";
-     STEAM_EXTRA_COMPT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
-  };
-  qt.enable = true;
-  gtk.enable = true;
+	# Home Manager can also manage your environment variables through
+	# 'home.sessionVariables'. If you don't want to manage your shell through Home
+	# Manager then you have to manually source 'hm-session-vars.sh' located at
+	# either
+	#
+	#  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+	#
+	# or
+	#
+	#  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+	#
+	# or
+	#
+	#  /etc/profiles/per-user/saturnfulcrum/etc/profile.d/hm-session-vars.sh
+	#
+	home.sessionVariables = {
+		EDITOR = "neovim";
+		STEAM_EXTRA_COMPT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
+	};
+	qt.enable = true;
+	gtk.enable = true;
 
-  programs.bash = {
-    enable = true;
-    shellAliases = {
-      edit_nix = "~/edit_nix.sh";
-      fuck = "/nix/store/11q8r4721nfqgn17gppl3s5wz5kzwayr-system-path/bin/thefuck";
-      ll = "ls -l";
-      la = "ls -a";
-      lla = "ls -l -a";
-      cdup = "cd ..";
-    };
-  };
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+	programs.bash = {
+		enable = true;
+		shellAliases = {
+			wmstart = "qtile start -b wayland";
+			list = "eza";
+			print = "bat";
+			hint = "tldr";
+		};
+		PS1 = "[\u@\h \W]\$";
+	};
+	# Let Home Manager install and manage itself.
+	programs.home-manager.enable = true;
   
 }
